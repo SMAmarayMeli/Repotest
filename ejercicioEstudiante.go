959b7970e66cc3e1bd8e9300237afead21051ebe
SMAmarayMeli <sebastian.samaray@mercadolibre.com>
2022-12-22 12:13:03 -0300

    Ejercicios modulo 4 primer acercamiento

---
+++ ejercicioEstudiante.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	var apellido string = "Gomez"
+	var edad int = 35
+	var tipo bool = false
+	var sueldo string = "$45857.90"
+	var nombre string = "Julián"
+
+	println(apellido, edad, tipo, sueldo, nombre)
+}
---
+++ ejercicioProfesor.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	var nombre string
+	var apellido string
+	var edad int
+	apellido = "Pepe"
+	var licencia_de_conducir = true
+	var estatura int
+	cantidadDeHijos := 2
+
+	println(nombre, apellido, edad, licencia_de_conducir, estatura, cantidadDeHijos)
+}
