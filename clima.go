fdb65adb2d212116313484ce8c6ecc8b7d4b1a22
SMAmarayMeli <sebastian.samaray@mercadolibre.com>
2022-12-22 10:54:34 -0300

    Ejercicio clima

---
+++ clima.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		temperatura = 5
+		humedad     = 15
+		presion     = 8
+	)
+
+	fmt.Println("Temperatura: ", temperatura, " Humedad: ", humedad, " Presion: ", presion)
+}
