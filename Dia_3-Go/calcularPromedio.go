12a43fa047550138a340345b4e41e10822dc7155
SMAmarayMeli <sebastian.samaray@mercadolibre.com>
2022-12-23 12:14:59 -0300

    Calcular promedio

---
+++ Dia_3-Go/calcularPromedio.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	notas := make([]int, 0)
+	nota := 1
+	for nota != 0 {
+		fmt.Println("Ingresar notas: ")
+		fmt.Scanln(&nota)
+
+		for nota < 0 || nota > 10 {
+			fmt.Println("Ingrese nota correctamente")
+			fmt.Println("Ingresar notas: ")
+			fmt.Scanln(&nota)
+		}
+		notas = append(notas, nota)
+	}
+
+	fmt.Println("Promedio: ", promedio(notas))
+}
+
+func promedio(notas []int) float64 {
+	var num int
+	for _, v := range notas {
+		num += v
+	}
+	return float64(num) / float64(len(notas))
+}
