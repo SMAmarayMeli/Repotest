58d88a5d9ebcc4b6dd93bca2fb00fb84a2d0b774
SMAmarayMeli <sebastian.samaray@mercadolibre.com>
2022-12-28 20:02:22 -0300

    Update

--- Dia_5-Go/Errors/ej1/impuestoDeSalario.go
+++ Dia_5-Go/Errors/ej1/impuestoDeSalario.go
@@ -5,17 +5,17 @@ import "fmt"
 type myerror struct {
 }
 
-func (e *myerror) Error() string {
+func (e *myerror) Error() (err string) {
 	return fmt.Sprintf("Error: el salario ingresado no alcanza el minimo imponible")
 }
 
 func main() {
 	var salary int
-	var err *myerror
 
 	fmt.Scanln(&salary)
 
-	if salary < 150000 {
-		fmt.Println(err)
+	if salary < 15000 {
+		fmt.Print()
 	}
+
 }
